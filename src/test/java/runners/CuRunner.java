971a9ecec6f35f44ed5baa5ed488a0219fa12341
anthonyspano <adspano91@gmail.com>
2022-04-01 15:12:48 -0600

    junit report

--- src/test/java/runners/CuRunner.java
+++ src/test/java/runners/CuRunner.java
@@ -7,12 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        plugin = {
-//                "json:target/cucumber.json",
-//                "html:target/cucumber/",
-//                "junit:target/junit/junit-report.xml",
-//                "rerun:target/rerun.txt"},
-        plugin = {"json:target/cucumber.json"},
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "stepDefinitions"
 )
